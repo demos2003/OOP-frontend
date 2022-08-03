52b659044d21f0bf2e8802a22a97a5c673a66d94
Jola <olusanyajolaoluwa@gmail.com>
2022-08-03 16:36:55 +0100

    CREATE BANNER

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import "./header.css"
-
-
+import "./header.css";
 
 export const Header = () => {
   return (
-    <div className='body'>1
-    <div className="navbar">
-      <div className="logo">
-        <img
-          src="https://res.cloudinary.com/manlikeemma/image/upload/v1652779315/Farm/HHF_Logo_um8xyr.png"
-          className="logo-size"
-          alt=""
-        />
+    <>
+      <div className="navbar">
+        {/* <div className="logo">
+          <img
+            src="https://res.cloudinary.com/manlikeemma/image/upload/v1652779315/Farm/HHF_Logo_um8xyr.png"
+            className="logo-size"
+            alt=""
+          />
+        </div> */}
+
+        <div className="navbar-btn navbar-btn1">
+          <Link to="/" className="nav_btn">
+            <a>Home</a>
+          </Link>
+          <Link to="/aboutus" className="nav_btn">
+            <a>Log in</a>
+          </Link>
+          <Link to="/product" className="nav_btn signup_btn">
+            <a>Sign up</a>
+          </Link>
+        </div>
       </div>
-      <div className="navbar-btn navbar-btn1">
-        <Link to="/" className="nav_btn">
-          <a>Home</a>
-        </Link>
-        <Link to="/aboutus" className="nav_btn">
-          <a>Login</a>
-        </Link>
-        <Link to="/product" className="nav_btn signup_btn">
-          <a>Sign up</a>
-        </Link>
-        
-     
-             </div>
-    </div>
-    </div>
-  )
-}
+    </>
+  );
+};
--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../Components/Header'
+import {Main} from '../Components/Main'
 
 const Landing = () => {
   return (
     <div className='body'>
     <Header/>
+    <Main/>
     </div>
   )
 }
