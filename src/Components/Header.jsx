030a1abb29eac0a999fe2d6401746f84df6ccaf6
Jola <olusanyajolaoluwa@gmail.com>
2022-08-06 16:10:10 +0100

    Complete user login

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./header.css"
-import Popup from './Popup';
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-
-
+import "./header.css";
+import Popup from "./Popup";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export const Header = () => {
-  const [buttonPopup, setButtonPopup ] = useState(false);
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <>
       <div className="navbar">
@@ -24,27 +22,53 @@ export const Header = () => {
           <Link to="/" className="nav_btn">
             <a>Home</a>
           </Link>
-          <a onClick={() => setButtonPopup(true)} className=' nav_btn'>Login</a>
+          <a onClick={() => setButtonPopup(true)} className=" nav_btn">
+            Login
+          </a>
           <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-      <form>
-            <h2>Edit Employee Info</h2>
-        <div class="form-row">
-         <div class="form-group form-edit">
-              <label for="inputEmail4">Email</label>
-              <input type="email" class="form-control input-edit" id="inputAddress" placeholder="Email Address"></input>
-          </div>
-          <div class="form-group">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" id="inputPassword4" placeholder="Password"></input>
-          </div>
-      </div>
-       
-       
-       
-        <button type="submit" class="btn btn-primary save-btn">Save</button>
-      </form>
-    </Popup>
-         
+            <form className="login_form">
+              <h1 className="login_title">Login</h1>
+              <div style={{ float: "left", width: "36%" }} className="line">
+                <hr />
+              </div>
+              <div style={{ float: "right", width: "36%" }}>
+                <hr />
+              </div>
+              <p className="inline_text">Sign up with Email</p>
+              <div class="form-row">
+                <div class="form-group form-edit loginfield">
+                  <label for="inputEmail4">Email</label>
+                  <div className="space"></div>
+                  <input
+                    type="email"
+                    class="form-control input-edit"
+                    id="inputAddress"
+                    placeholder="Email Address"
+                  ></input>
+                </div>
+                <div class="form-group loginfield">
+                  <label for="inputPassword4">Password</label>
+                  <div className="space"></div>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="inputPassword4"
+                    placeholder="Password"
+                  ></input>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary login-btn">
+                Log in
+              </button>
+              <p className="signup_link">
+                Not registered yet?
+                <Link to="/signup" style={{ color: "#ff3259" }}>
+                  Create an account
+                </Link>
+              </p>
+            </form>
+          </Popup>
+
           <Link to="/signup" className="nav_btn signup_btn">
             <a href="">Sign up</a>
           </Link>
