ec35583bb4995922f6fd5cbbd1aff722a1842e10
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2022-08-14 19:16:57 +0100

    Added Footer to client page

--- src/Views/ClientHome.jsx
+++ src/Views/ClientHome.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ClientNav } from "../Components/ClientNav";
 import { ClientHomepage } from "../Components/ClientHomepage";
+import { Footer } from "../Components/Footer";
 
 const ClientHome = () => {
   return (
     <div className="body">
       <ClientNav />
       <ClientHomepage />
+      <Footer />
     </div>
   );
 };
