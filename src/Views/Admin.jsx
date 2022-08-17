1507c5dde8c550b0ee03ee53e786361720096ea1
Jola <olusanyajolaoluwa@gmail.com>
2022-08-17 15:50:14 +0100

    style request

--- src/Views/Admin.jsx
+++ src/Views/Admin.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import AdminNav from '../Components/AdminNav'
-import './admin.css'
-import Requests from '../Components/Requests'
-import { useState } from 'react'
-import AdminEventCenters from '../Components/AdminEventCenters'
-
+import React from "react";
+import AdminNav from "../Components/AdminNav";
+import "./admin.css";
+import Requests from "../Components/Requests";
+import { useState } from "react";
+import AdminEventCenters from "../Components/AdminEventCenters";
+import { BiMessage} from "react-icons/bi";
+import { BsCalendar4Event } from "react-icons/bs";
+import {AiOutlineStar} from "react-icons/ai"
+import {RiDraftLine} from "react-icons/ri"
 const Admin = () => {
-  const [active1, setActive1] = useState("Request")
+  const [active1, setActive1] = useState("Request");
   return (
-    <div className='color'>
-      <AdminNav/>
-      <div className='admin_panelHolder'>
-        <div className='sidebar'>
-            <p>
-              <a onClick={() => setActive1("Request")}>
-              Requests
-              </a></p>
-              <p>
-                <a onClick={() => setActive1("Events")}>
-                Events Centers
-                </a>
-                </p>
+    <div className="color">
+      <AdminNav />
+      <div className="admin_panelHolder">
+        <div className="sidebar">
+          <p className="request-btn">
+            <a onClick={() => setActive1("Request")}>
+              <b><BiMessage/></b>
+              <b className="space">Requests</b>
+            </a>
+          </p>
+          <p className="event_centers-btn">
+            <a onClick={() => setActive1("Events")}>
+              <b><BsCalendar4Event /></b>
+              <b className="space">Events Centers</b>
+            </a>
+          </p>
+          <p className="event_centers-btn">
+            <a onClick={() => setActive1("Events")}>
+              <b><AiOutlineStar/></b>
+              <b className="space">Starred Event</b>
+            </a>
+          </p>
+          <p className="event_centers-btn">
+            <a onClick={() => setActive1("Events")}>
+              <b><RiDraftLine/></b>
+              <b className="space">Draft</b>
+            </a>
+          </p>
         </div>
-        <div className='admin_request'>
-          {active1 === "Request" && <Requests/>}
-           {active1 === "Events" && <AdminEventCenters/>} 
+        <div className="admin_request">
+          {active1 === "Request" && <Requests />}
+          {active1 === "Events" && <AdminEventCenters />}
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
