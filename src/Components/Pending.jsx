9e2c6e1cec8a0593007c6e29d1743dd55a85fc7f
Alabi <alabiemmanuel177@gmail.com>
2022-08-22 23:03:09 +0100

    version 1

--- src/Components/Pending.jsx
+++ src/Components/Pending.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import './adminNav.css'
+import React from "react";
+import "./adminNav.css";
 
-
-const Pending = ({ active1, setActive1, bookData, active2, setActive2, }) => {
-   const Pending2 = ({ book }) => {
-      // setActive2(book._id)
-      return (
-         <div>
-            <div className='request_info' onClick={() => setActive1("Test")} >
-               <p>{book.username}{setActive2(book._id)
-               }</p>
-               <p>{book.eventcenter}
-               </p>
-               <p>{book.duration}</p>
-               <p>{book.desc}</p>
-               <p>{book.dateofbooking}</p>
-               <p>{book.duration}</p>
-            </div>
-         </div>
-      )
-   }
-
-   return (
-      <div className='requestContent_holder'>
-         {bookData.map((p) => (
-            <Pending2 book={p} />
-         ))}
+const Pending = ({ active1, setActive1, bookData, active2, setActive2 }) => {
+  const Pending2 = ({ book }) => {
+    return (
+      <div>
+        <div className="request_info" onClick={() => setActive1("Test")}>
+          <p>
+            {book.fullname}
+            {setActive2(book._id)}
+          </p>
+          <p>{book.email}</p>
+          <p>0{book.phoneno}</p>
+          <p>{book.eventcenter}</p>
+          <p>{book.eventname}</p>
+          <p>{book.duration}</p>
+          <p>{book.dateofevent}</p>
+          <p>{book.duration} hour(s)</p>
+        </div>
       </div>
-   )
-}
+    );
+  };
 
-export default Pending
+  return (
+    <div className="requestContent_holder">
+      {bookData.map((p) => (
+        <Pending2 book={p} />
+      ))}
+    </div>
+  );
+};
+
+export default Pending;
