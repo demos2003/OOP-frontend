ffa84c65a55b2cf65fc03ec51b4d0444f832cede
Alabi <alabiemmanuel177@gmail.com>
2022-08-23 21:40:47 +0100

    fixes

--- src/Components/Declined.jsx
+++ src/Components/Declined.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Declined = ({deniedData}) => {
-    const Denied = ({denied}) => {
-        return(
-            <div className='request_info'>
-                <p>{denied.username}</p>
-            <p>{denied.eventcenter}
-            </p>
-            <p>{denied.duration}</p>
-            <p>{denied.desc}</p>
-            <p>{denied.dateofbooking}</p>
-            <p>{denied.duration}</p>
-            </div>
-        );
-    }
+const Declined = ({ deniedData }) => {
+  const Denied = ({ denied }) => {
     return (
-        <div className='requestContent_holder'>
-          {deniedData.map((p) =>(
-              <Denied denied={p}/>
-          ))}
-           
-            
-        </div>
-    )
-}
+      <div className="request_info">
+        <p>{denied.fullname}</p>
+        <p>{denied.email}</p>
+        <p>{denied.phoneno}</p>
+        <p>{denied.eventcenter}</p>
+        <p>{denied.duration}</p>
+        <p>{denied.dateofevent}</p>
+        <p>{denied.duration} hour(s)</p>
+      </div>
+    );
+  };
+  return (
+    <div className="requestContent_holder">
+      {deniedData.map((p) => (
+        <Denied denied={p} />
+      ))}
+    </div>
+  );
+};
 
-export default Declined
+export default Declined;
