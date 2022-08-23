f3eb8293e522553324505a0edb1d02f237c44d8c
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2022-08-23 17:35:19 +0100

    Minor changes

--- src/Components/UpcomingEventsTab.jsx
+++ src/Components/UpcomingEventsTab.jsx
@@ -3,7 +3,7 @@ import "./ClientHomepage.css";
 
 const UpcomingEventsTab = () => {
   return (
-    <div>
+    <div className="UpcomingEventsTab">
       <div className="event-container">
         <div className="event-image-container">
           <img
@@ -12,7 +12,7 @@ const UpcomingEventsTab = () => {
             className="event-image"
           />
         </div>
-        <div className="event-date">20, August 2022 | 4 hours</div>
+        <div className="event-date">20, August 2022 | 14:00 | 4 hours</div>
         <div className="event-name">Freshers Fest 2022</div>
         <div className="event-location">
           <div className="location-block">
